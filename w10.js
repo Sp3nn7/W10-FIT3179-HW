0948b5ade8dd8c6d908cc3d5ad2867e8738ba1ab
Spenc <sger0007@student.monash.edu>
2023-10-08 20:09:14 +1100

    adjusting our stacked bar chart

--- w10.js
+++ w10.js
@@ -1,4 +1,4 @@
-var vg_1 = "word_cloud.vg.json";
+var vg_1 = "w10.vg.json";
 vegaEmbed("#w10_idiom", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
