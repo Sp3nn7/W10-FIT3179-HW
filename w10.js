65fd012916009d9eeb7b61aa77a489ddcd422bf1
Spenc <sger0007@student.monash.edu>
2023-10-08 20:36:43 +1100

    adjusting the visualization format

--- w10.js
+++ w10.js
@@ -1,4 +1,11 @@
 var vg_1 = "w10.vg.json";
-vegaEmbed("#w10_idiom", vg_1).then(function(result) {
+var vg_2 = "w10-dchart.vg.json";
+
+
+vegaEmbed('#vis3', vg_2, { "actions": false }).then(function (result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed('#vis4', vg_1, { "actions": false }).then(function (result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
